133e1748be69a81f211ccdcba9dcab0317edb088
kostiago <tiago_pb-@hotmail.com>
2024-09-19 15:35:31 -0300

    feat: finalizing the initial settings, adding preload for the svg, icons.

---
+++ app/app/preload-resources.tsx
@@ -0,0 +1,10 @@
+"use client";
+import ReactDOM from "react-dom";
+
+export function PreloadResources() {
+  ReactDOM.preload("./sprite.svg", {
+    as: "image",
+  });
+
+  return null;
+}
---
+++ app/components/icons.tsx
@@ -0,0 +1,31 @@
+import { SVGProps } from "react";
+
+type IconName = "edit" | "eye" | "link" | "location";
+
+type IconProps = Readonly<{
+  name: IconName;
+  children?: React.ReactNode;
+  childrenClassName?: string;
+}>;
+
+export function Icon({
+  name,
+  children,
+  childrenClassName,
+  ...props
+}: SVGProps<SVGSVGElement> & IconProps) {
+  if (children) {
+    return (
+      <span className={`inline-flex items-center gap-1 ${childrenClassName}`}>
+        <Icon name={name} {...props} />
+        {children}
+      </span>
+    );
+  }
+
+  return (
+    <svg {...props}>
+      <use href={`./sprite.svg#${name}`} />
+    </svg>
+  );
+}
